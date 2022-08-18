32fee407e0540d8e48e6f928b15864e9045a56e8
YashChouhan395 <104978374+YashChouhan395@users.noreply.github.com>
2022-08-18 23:54:50 +0530

    Update OrderController.java
    Signed-off-by: YashChouhan395 <104978374+YashChouhan395@users.noreply.github.com>

--- Doctorbackend/src/main/java/com/doctor/demo/controller/OrderController.java
+++ Doctorbackend/src/main/java/com/doctor/demo/controller/OrderController.java
@@ -1,42 +1,42 @@
-package com.doctor.demo.controller;
+// package com.doctor.demo.controller;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.doctor.demo.repository.Orders;
-import com.doctor.demo.service.OrdersService;
+// import com.doctor.demo.repository.Orders;
+// import com.doctor.demo.service.OrdersService;
 
-@Controller
-@RequestMapping("api/orders")
-public class OrderController {
-	@Autowired
-	OrdersService ordersservice;
+// @Controller
+// @RequestMapping("api/orders")
+// public class OrderController {
+// 	@Autowired
+// 	OrdersService ordersservice;
 	
-	@GetMapping("/get")
-	public @ResponseBody List<Orders> getAllOrders(){
-		return ordersservice.getAllOrders();
-	}
+// 	@GetMapping("/get")
+// 	public @ResponseBody List<Orders> getAllOrders(){
+// 		return ordersservice.getAllOrders();
+// 	}
 	
-	// post
-	@PostMapping("/postorder")
-	public @ResponseBody Orders postOrders(@RequestBody Orders order) {
-		return ordersservice.postOrders(order);
-	}
+// 	// post
+// 	@PostMapping("/postorder")
+// 	public @ResponseBody Orders postOrders(@RequestBody Orders order) {
+// 		return ordersservice.postOrders(order);
+// 	}
 	
-	//delete
-	 @DeleteMapping("/deleteorder/{id}")
-	 public void deleteOrder(@PathVariable Integer id ) {
-		 ordersservice.deleteOrders(id);
-	 }
+// 	//delete
+// 	 @DeleteMapping("/deleteorder/{id}")
+// 	 public void deleteOrder(@PathVariable Integer id ) {
+// 		 ordersservice.deleteOrders(id);
+// 	 }
 //	 
 //	 @GetMapping("getbyid/{username}")
 //	 public @ResponseBody List<Orders> getOrderById(@PathVariable String username){
