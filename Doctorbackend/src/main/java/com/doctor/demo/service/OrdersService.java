86a468ec1e5099f92cfeb9fcf1281b3dce201a6f
YashChouhan395 <104978374+YashChouhan395@users.noreply.github.com>
2022-08-18 23:55:21 +0530

    Update OrdersService.java
    Signed-off-by: YashChouhan395 <104978374+YashChouhan395@users.noreply.github.com>

--- Doctorbackend/src/main/java/com/doctor/demo/service/OrdersService.java
+++ Doctorbackend/src/main/java/com/doctor/demo/service/OrdersService.java
@@ -1,33 +1,33 @@
-package com.doctor.demo.service;
+// package com.doctor.demo.service;
 
-import java.util.List;
+// import java.util.List;
 
-import org.springframework.stereotype.Service;
+// import org.springframework.stereotype.Service;
 
-import com.doctor.demo.repository.Orders;
+// import com.doctor.demo.repository.Orders;
 
 
 
-public interface OrdersService {
-	public List<Orders> getAllOrders();
+// public interface OrdersService {
+// 	public List<Orders> getAllOrders();
 	
-	public Orders postOrders(Orders order);
+// 	public Orders postOrders(Orders order);
 	
-//	public Orders updateOrders(Orders order, Integer orderid);
+// //	public Orders updateOrders(Orders order, Integer orderid);
 	
-	public void deleteOrders(Integer orderid);
+// 	public void deleteOrders(Integer orderid);
 	
-// get by id
+// // get by id
 	
-	public List<Orders> findByUsername(String id);
+// 	public List<Orders> findByUsername(String id);
 	
-// revenue
+// // revenue
 	
-	public Long calculateRevenue();
+// 	public Long calculateRevenue();
 	
-// orders by username
+// // orders by username
 	
-	public List<Orders> findOrdersByUsername(String username);
+// 	public List<Orders> findOrdersByUsername(String username);
 	
 
-}
+// }
